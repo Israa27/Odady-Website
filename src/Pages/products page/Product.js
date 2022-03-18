ee72469a689b820e93e1fa82639132c68a25b2da
Israa27 <israa.adnan.1997@gmail.com>
2022-03-18 14:24:52 +0300

    some changes -_-

--- src/Pages/products page/Product.js
+++ src/Pages/products page/Product.js
@@ -1,79 +1,108 @@
-import React, { useState ,useEffect}  from 'react';
-import ProductCard from '../../Components/Card/ProductCard';
+import React, { useState ,useEffect, useCallback}  from 'react';
 import SecondaryNav from '../../Components/secondary navbar/SecondaryNav';
-import { Container } from 'react-bootstrap';
-import SpinnerLoading from '../../Components/spinner/SpinnerLoading'
 import './product.css';
+import Products from './products section/Products';
+import Filters from './filtertion/Filters';
+import {  useSelector ,useDispatch} from 'react-redux';
+import { viweAllProducts } from '../../redux/showAllSlice';
 import PaginationPage from './Pagination/PaginationPage';
-import {  useSelector } from 'react-redux';
 export default function Product() {
-    
-    const products=JSON.parse(localStorage.getItem("allProducts"));
-    const status=useSelector((state)=> state.all);
+  const products=useSelector((state)=> state.all.all_products); 
+  //fatch all products
+  console.log(products)
+  
+  const[value,setValue]=useState(''); //company
+  const[cate,setCate]=useState(''); //category
+  const[sort,setSort]=useState(''); //sort
+  const[filters,setFilters]=useState(products);
+  const[currentPage,setCurrentPage]=useState(1);
+  const[productsPerPage]=useState(5);
+  //filter by company name
+  const dispatch = useDispatch();
+  const handleFilter=(val)=>{
+     setValue(val);
+     console.log(val)
+     const company=products.filter((companyItem)=>{
+       return companyItem.company.name===val
+       
+     })
+     setFilters( company)
+    }
 
+     //filter by category name
+     const handleFilter2=(val)=>{
+      setCate(val);
+      
+      const category=products.filter((categoryItem)=>{
+        return categoryItem.category.name===val
+        
+      })
+      
+      setFilters(category)
 
-    //sort 
-    const[sort,setSort]=useState([]);
-    const[fliterProducts,setFilterProducts]=useState([]);
-
-    //pagination
-    const[currentPage,setCurrentPage]=useState(1);
-    const itemPerPage=5;
-    const pages=[];
-    for(let i=1;i<=Math.ceil(products.length/itemPerPage);i++){
-        pages.push(i);
     };
-    const indexOfLastItem=currentPage*itemPerPage;
-    const indexOfFisrtItem=indexOfLastItem-itemPerPage;
-    const filterItems=fliterProducts.slice(indexOfFisrtItem,indexOfLastItem);
-  //pagination
-
-
- useEffect(()=>{
-  if (sort === "asc") {
-    const lowestPrice = products
-      .map((product) => product)
-      .sort((a, b) => a.price - b.price);;
-    setFilterProducts(lowestPrice);
-   
-  }
- else{
-  const lowestPrice = products
-      .map((product) => product)
-      .sort((a, b) => b.price - a.price);;
-    setFilterProducts(lowestPrice);
     
-  }
- },[sort])
+   useCallback(()=>{
+    const category=products.filter((categoryItem)=>{
+      return categoryItem.category.name===val
+      
+    })
+    setFilters(category)
+    },[filters])
+         
+    
+  
+      //sort products
+     useEffect(()=>{
+      if (sort === "asc") {
+        const lowestPrice = products
+          .map((product) => product)
+          .sort((a, b) => a.price - b.price);;
+        setFilters(lowestPrice);
+       
+      }
+     else{
+      const lowestPrice = products
+          .map((product) => product)
+          .sort((a, b) => b.price - a.price);;
+        setFilters(lowestPrice);
+        
+      }
+     
+     
+     },[setFilters,sort])
+     
+    //pagination 
+    const indexOfLastProduct = currentPage * productsPerPage;
+    const indexOfFirstProduct = indexOfLastProduct - productsPerPage;
+    const currentProducts = filters.slice(indexOfFirstProduct, indexOfLastProduct);
+    const totalPages = Math.ceil(filters.length / productsPerPage);
 
+  const showAll=()=>{
+   dispatch(viweAllProducts())
+  }
 
   return <div>
-    { status === 'pending' ?(
-       
-       <SpinnerLoading/>
-    ):(
-      <>
        <SecondaryNav name='المنتجات'/>
-         <Container>
-          <div className='filter-items'>
-            <select onChange={(e)=>setSort(e.target.value)}>
-              <option  >الترتيب الافتراضي</option>
-              <option value='asc'>ترتيب حسب: الأدنى سعراً للأعلى</option>
-              <option value='desc'>ترتيب حسب: الأعلى سعراً للأدنى</option>
-            </select>
-          </div>
-          <div className='list-items'>
-    
-          {filterItems.slice(0,4).map((item)=>{
-              
-              return <ProductCard product={item} id={item.id} name={item.name} image={item.images } price={item.price} key={item.id} />
-            })}
-          
-          </div>
-          
-         <PaginationPage pages={pages}currentPage={currentPage} setCurrentPage={setCurrentPage}/>
-    </Container>
-  </>
-    )}
+         <div className='flex-content'>
+          <Filters 
+          value={value}
+          cate={cate}
+          sort={sort}
+          showAll={showAll}
+          handleFilter={handleFilter}
+          handleFilter2={handleFilter2}
+          setSort={setSort}
+          />
+          <Products 
+          currentProducts={currentProducts}
+          filters={filters}
+          />
+      
+        </div>
+        <PaginationPage 
+        setCurrentPage={setCurrentPage} 
+        pages = {totalPages}
+        />
   </div>;
 }
