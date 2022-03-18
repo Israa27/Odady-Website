ba41636b40d1c1027a69eac628902bef333d50d8
Israa27 <israa.adnan.1997@gmail.com>
2022-03-18 14:47:53 +0300

    some changes

--- src/Pages/products page/filtertion/Filters.js
+++ src/Pages/products page/filtertion/Filters.js
@@ -3,7 +3,7 @@ import './filter.css'
 
 
    
-export default function Filters({showAll,value,cate,sort,handleFilter,handleFilter2,setSort}) {
+export default function Filters({showAll,value,cate,sort,setValue,setCate,setSort}) {
   
   return (
     <div className='filter-card'>
@@ -14,15 +14,15 @@ export default function Filters({showAll,value,cate,sort,handleFilter,handleFilt
           <span>حسب الشركة</span>
           
           <div className='radio'>
-             <input type='radio' id='radio1' value='Caterpillar' onChange={(e)=>handleFilter(e.target.value)} checked={value ==='Caterpillar'}/>
+             <input type='radio' id='radio1' value='Caterpillar' onChange={(e)=>setValue(e.target.value)} checked={value ==='Caterpillar'}/>
              <label htmlFor='radio1'>شركة Caterpillar</label>  
          </div>
          <div className='radio'>
-            <input type='radio' id='radio2' value='شركة INGCO الصينية' onChange={(e)=>handleFilter(e.target.value)} checked={value ==='شركة INGCO الصينية'}/>
+            <input type='radio' id='radio2' value='شركة INGCO الصينية' onChange={(e)=>setValue(e.target.value)} checked={value ==='شركة INGCO الصينية'}/>
             <label htmlFor='radio2'>شركة INGCO الصينية</label>
          </div>
          <div className='radio'>
-            <input type='radio' id='radio3' value='شركة CROWN السويسرية' onChange={(e)=>handleFilter(e.target.value)} checked={value ==='شركة CROWN السويسرية'}/>
+            <input type='radio' id='radio3' value='شركة CROWN السويسرية' onChange={(e)=>setValue(e.target.value)} checked={value ==='شركة CROWN السويسرية'}/>
             <label htmlFor='radio3'>شركة CROWN السويسرية</label>
          </div>
          <hr/>
@@ -31,19 +31,19 @@ export default function Filters({showAll,value,cate,sort,handleFilter,handleFilt
         <span>حسب الفئات</span>
          
         <div className='radio'>
-           <input type='radio' id='radio4' value='اجهزة كهربائية' onChange={(e)=>handleFilter2(e.target.value)} checked={cate ==='اجهزة كهربائية'} />
+           <input type='radio' id='radio4' value='اجهزة كهربائية' onChange={(e)=>setCate(e.target.value)} checked={cate ==='اجهزة كهربائية'} />
             <label htmlFor='radio4'>اجهزة كهربائية </label>  
          </div>
          <div className='radio'>
-            <input type='radio' id='radio5' value='عدد يدوية' onChange={(e)=>handleFilter2(e.target.value)} checked={cate ==='عدد يدوية'}/>
+            <input type='radio' id='radio5' value='عدد يدوية' onChange={(e)=>setCate(e.target.value)} checked={cate ==='عدد يدوية'}/>
             <label htmlFor='radio5'>عدد يدوية</label>
          </div>
          <div className='radio'>
-            <input type='radio' id='radio6' value='ادوات لحام' onChange={(e)=>handleFilter2(e.target.value)} checked={cate ==='ادوات لحام'}/>
+            <input type='radio' id='radio6' value='ادوات لحام' onChange={(e)=>setCate(e.target.value)} checked={cate ==='ادوات لحام'}/>
             <label htmlFor='radio6'>ادوات لحام</label>
          </div>
          <div className='radio'>
-            <input type='radio' id='radio7' value='اجهزة شحن' onChange={(e)=>handleFilter2(e.target.value)} checked={cate ==='اجهزة شحن'}/>
+            <input type='radio' id='radio7' value='اجهزة شحن' onChange={(e)=>setCate(e.target.value)} checked={cate ==='اجهزة شحن'}/>
             <label htmlFor='radio7'>اجهزة شحن</label>
          </div>
          <hr/>
