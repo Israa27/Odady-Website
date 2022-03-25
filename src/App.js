4f6bd7ab602d55394310b0eb5eee41795aad0b2d
Israa27 <israa.adnan.1997@gmail.com>
2022-03-25 22:15:37 +0300

    edit profile

--- src/App.js
+++ src/App.js
@@ -1,47 +1,28 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import './App.css';
-
-import Register from './Pages/register Page/Register';
+import routes from './routes';
 import { Route, Routes, useLocation } from 'react-router-dom';
-import Profile from './Pages/profile page/Profile';
-import ForgetPassword from './Pages/forget password/ForgetPassword';
-import ResetPassword from './Pages/reset password/ResetPassword';
-import LogIn from './Pages/login page/LogIn';
-import Layouts from './layouts/Layouts'
-import NotFound from './Pages/not found page/NotFound';
+import SpinnerLoading from './Components/spinner/SpinnerLoading';
 
 
 function App() {
   
   const location=useLocation()
   return<div className="App">
-    
-    
+   <Suspense fallback={<SpinnerLoading/>}>
+      
       <Routes >
-          <Route exact path="/" element={<Layouts/> }/>
-          <Route exact path="/register" element={<Register />}/>
-          <Route exact path="/login" element={<LogIn />}/>
-          <Route exact path="/products" element={<Layouts />}/>
-          <Route exact path="/product_detiles" element={<Layouts />}/>
-          
-        
-          <Route exact path="/cart" element={<Layouts />}/>
-          <Route exact path="/checkout" element={<Layouts/>}/>
-          <Route exact path="/contact" element={<Layouts />}/>
-          <Route exact path="/wishlist" element={<Layouts />}/>
-         
-          <Route exact path="/privacy" element={<Layouts />}/>
-          <Route exact path="/transfer" element={<Layouts />}/>
-          <Route exact path="/profile_user" element={<Profile />}/>
-            <Route exact path="/account" element={<Profile />}/>
-            <Route exact path="/myorder" element={<Profile />}/>
-            <Route exact path="/mywishlist" element={<Profile />}/>
-          <Route exact path="/forgetpassword" element={<ForgetPassword/>}/>
-          <Route exact path="/resetpassword" element={<ResetPassword/>}/>
-          <Route exact path="/about" element={<Layouts />}/>
-          <Route exact path='/not_found' element={<NotFound/>}/>
-   
+         {routes.map((name,key)=>{return(
+           <Route 
+           key={key}
+           exact={name.exact}
+           path={name.path}
+           element={<name.element/>}
+           />
+           
+         )})}
       </Routes>
+      </Suspense>
       </div>
       
             
--- src/redux/order/orderSlice.js
+++ src/redux/order/orderSlice.js
@@ -1,58 +1,30 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 import Swal from 'sweetalert2'
-import { toast } from 'react-toastify';
 import {BASE_URL} from '../../Helpers/Constants';
-const URL=BASE_URL+'/orders/use_coupon';
+
 
 const initialState ={
   status:'',
   coupon:[],
-  isLoading:false
+  orderId:[],
+  orderlist:[],
+  orderInfo:localStorage.getItem('productInfo')?JSON.parse(localStorage.getItem('productInfo')):[],
+  isLoading:false,
+  
 
 }
 
 
 
 
-//get  best seller products
-export const addToCart = createAsyncThunk(
-    "cart/add_to_cart",
-    async (id,thunkAPI)=> {
-     const {dispatch}=thunkAPI
-      
-      try {
-        const response = await axios.post(URL,{
-        product_id: id,
-        
-    }
-      )
-      Swal.fire({
-        icon: 'success',
-        title: 'تمت الاضافة بنجاح',
-        text: 'تمت اضافة منتج الى سلة المشتريات',
-       
-      });
-      
-       return response.data
-      
-    } catch (error) {
-      Swal.fire({
-        icon: 'error',
-        title: 'Oops... -_-',
-        text: 'حدث خطأ ما !',
-        footer: '<a href="/">هل تريد الرجوع الى صفحة الرئيسة?</a>'
-      });
-    }
-  }
-);
-
+//get coupon 
 export const PostCoupon = createAsyncThunk(
   "order/get_coupon",
   async (code)=> {
     
     try {
-      const response = await axios.post(`${URL}?coupon_code=${code}`,{
+      const response = await axios.post(`${BASE_URL}/orders/use_coupon?coupon_code=${code}`,{
  
       }
     )  
@@ -75,11 +47,164 @@ export const PostCoupon = createAsyncThunk(
      return response.data
     
   } catch (error) {
-  
+    const Toast = Swal.mixin({
+      toast: true,
+      position: 'top-end',
+      showConfirmButton: false,
+      timer: 3000,
+      timerProgressBar: true,
+      didOpen: (toast) => {
+        toast.addEventListener('mouseenter', Swal.stopTimer)
+        toast.addEventListener('mouseleave', Swal.resumeTimer)
+      }
+    })
+    
+    Toast.fire({
+      icon: 'error',
+      title: 'الرمز غير صالح '
+    })
   }}
 );
 
+// create order
+export const CreateOrder = createAsyncThunk(
+  "order/create_order",
+  async (id)=> {
+    
+    try {
+      const response = await axios.post(id ?`${BASE_URL}/orders/create-order`:`${BASE_URL}/orders/create-order?coupon_id=${id}`,{
+ 
+      }
+    )  
+    const Toast = Swal.mixin({
+      toast: true,
+      position: 'top-end',
+      showConfirmButton: false,
+      timer: 3000,
+      timerProgressBar: true,
+      didOpen: (toast) => {
+        toast.addEventListener('mouseenter', Swal.stopTimer)
+        toast.addEventListener('mouseleave', Swal.resumeTimer)
+      }
+    })
+    
+    Toast.fire({
+      icon: 'success',
+      title: 'تمت العملية بنجاح '
+    })
+     return response.data
+    
+  } catch (error) {
+      console.log(error)
+  }}
+);
+// get order 
+export const getOrders = createAsyncThunk(
+  "order/get_orders",
+  async ()=> {
+    
+    try {
+      const response = await axios.get(`${BASE_URL}/orders/list_orders`,{
+ 
+      }
+    )  
+    const Toast = Swal.mixin({
+      toast: true,
+      position: 'top-end',
+      showConfirmButton: false,
+      timer: 3000,
+      timerProgressBar: true,
+      didOpen: (toast) => {
+        toast.addEventListener('mouseenter', Swal.stopTimer)
+        toast.addEventListener('mouseleave', Swal.resumeTimer)
+      }
+    })
+    
+    Toast.fire({
+      icon: 'success',
+      title: 'تمت العملية بنجاح '
+    })
+    return response.data
+    
+  } catch (error) {
+      console.log(error)
+  }}
+);
+// get order info
+export const getOrderInfo = createAsyncThunk(
+  "order/get_order_info",
+  async (id)=> {
+    
+    try {
+      const response = await axios.get(`${BASE_URL}/orders/order_info/${id}`,{
+ 
+      }
+    )  
+    const Toast = Swal.mixin({
+      toast: true,
+      position: 'top-end',
+      showConfirmButton: false,
+      timer: 3000,
+      timerProgressBar: true,
+      didOpen: (toast) => {
+        toast.addEventListener('mouseenter', Swal.stopTimer)
+        toast.addEventListener('mouseleave', Swal.resumeTimer)
+      }
+    })
+    
+    Toast.fire({
+      icon: 'success',
+      title: 'تمت العملية بنجاح '
+    })
+    localStorage.setItem('productInfo',JSON.stringify(response.data))
+    return response.data
+    
+  } catch (error) {
+      console.log(error)
+  }}
+);
+export const CheckOut = createAsyncThunk(
+  "order/checkout",
+  async (value)=> {
+    
+    try {
+      const response = await axios.post(`${BASE_URL}/orders/check_out`,{
+        Baghdad:value,
+      
+      }
+     
+    )  
+    Swal.fire({
+      icon: 'success',
+      title:'تم ارسال الطلب',
+      text: 'تم ارسال طلبك بنجاح ^_^',
 
+      
+      }).then(function() {
+      window.location = "/";
+    })
+     
+     return response.data
+    
+  } catch (error) {
+    const Toast = Swal.mixin({
+      toast: true,
+      position: 'top-end',
+      showConfirmButton: false,
+      timer: 3000,
+      timerProgressBar: true,
+      didOpen: (toast) => {
+        toast.addEventListener('mouseenter', Swal.stopTimer)
+        toast.addEventListener('mouseleave', Swal.resumeTimer)
+      }
+    })
+    
+    Toast.fire({
+      icon: 'error',
+      title: 'الرمز غير صالح '
+    })
+  }}
+);
 
 export const orderSlice = createSlice({
   name: 'order',
@@ -91,11 +216,13 @@ export const orderSlice = createSlice({
     [PostCoupon.pending]: (state, action) => {
         state.status = "pending"
         state.isLoading=true
+        state.error=''
   },
     [PostCoupon.fulfilled]: (state, action) => {
           state.isLoading=false
           state.coupon=[action.payload]
           state.status = "success"
+          state.error=''
   },
     [PostCoupon.rejected]: (state, action) => {
         state.status = "rejected";
@@ -103,7 +230,73 @@ export const orderSlice = createSlice({
         state.error='error'
   },
     
+// create order
+[CreateOrder.pending]: (state, action) => {
+    state.status = "pending"
+    state.isLoading=true
+    state.error=''
+},
+[CreateOrder.fulfilled]: (state, action) => {
+      state.isLoading=false
+      state.orderId=action.payload
+      state.status = "success"
+      state.error=''
+},
+[CreateOrder.rejected]: (state, action) => {
+    state.status = "rejected";
+    state.isLoading=false
+    state.error='error'
+},
+// get order list
+[getOrders.pending]: (state, action) => {
+  state.status = "pending"
+  state.isLoading=true
+  state.error=''
+},
+[getOrders.fulfilled]: (state, action) => {
+    state.isLoading=false
+    state.orderlist=action.payload
+    state.status = "success"
+    state.error=''
+},
+[getOrders.rejected]: (state, action) => {
+  state.status = "rejected";
+  state.isLoading=false
+  state.error='error'
+},
 
+[getOrderInfo.pending]: (state, action) => {
+  state.status = "pending"
+  state.isLoading=true
+  state.error=''
+},
+[getOrderInfo.fulfilled]: (state, action) => {
+    state.isLoading=false
+    state.orderInfo=action.payload 
+    state.status = "success"
+    state.error=''
+},
+[getOrderInfo.rejected]: (state, action) => {
+  state.status = "rejected";
+  state.isLoading=false
+  state.error='error'
+},
+//checkout
+[CheckOut.pending]: (state, action) => {
+  state.status = "pending"
+  state.isLoading=true
+  state.error=''
+},
+[CheckOut.fulfilled]: (state, action) => {
+    state.isLoading=false
+    state.status = "success"
+    state.error=''
+},
+[CheckOut.rejected]: (state, action) => {
+  state.status = "rejected";
+  state.isLoading=false
+  state.error='error'
+},
 
   
 
